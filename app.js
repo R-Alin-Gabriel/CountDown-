2ad26b20b90d30a8445afd033e28fe4de742306b
R-Alin-Gabriel <rudareanu_alin@yahoo.com>
2021-12-13 14:40:59 +0200

    ok

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ const countdown = () => {
     const textMinute = Math.floor((gap % hour) / minute);
     const textSecond = Math.floor((gap % minute) / second);
 
+    
     document.querySelector('.day').innerText = textDay;
     document.querySelector('.hour').innerText = textHour;
     document.querySelector('.minute').innerText = textMinute;
